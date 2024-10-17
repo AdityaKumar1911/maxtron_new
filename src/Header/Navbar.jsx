cf621811638dc593f75fbc5ca96fbd78136dee56
Aditya Kumar <kaditya.vit@gmail.com>
2024-10-17 17:08:08 +0530

    fix 5

--- src/Header/Navbar.jsx
+++ src/Header/Navbar.jsx
@@ -55,12 +55,6 @@ const Navbar = () => {
     navigate("/"); // Redirect to the homepage
   };
 
-  // Handle hamburger menu click for mobile view
-  const handleMenuClick = (e) => {
-    e.preventDefault(); // Prevent default link behavior
-    toggleMobileMenu();
-  };
-
   return (
     <nav className="navbar">
       <div className="navbar-left">
@@ -80,12 +74,20 @@ const Navbar = () => {
                 {isScooterDropdownOpen && (
                   <div className="dropdown-content animated-dropdown">
                     <div className="dropnew">
+                      {/* <a href="#scooter1">Scooter Model 1</a>
+                      <a href="#scooter2">Scooter Model 2</a>
+                      <a href="#scooter3">Scooter Model 3</a>
+                      <a href="#scooter3">Scooter Model 4</a> */}
                       <ScooterData />
                     </div>
                   </div>
                 )}
               </div>
             </div>
+
+            {/* <a id="offerHide" href="#offer" className="nav-link">
+              Offer
+            </a> */}
           </>
         )}
       </div>
@@ -108,14 +110,31 @@ const Navbar = () => {
         {/* Always show this icon in both desktop and mobile views */}
         <div style={{ marginRight: "20px" }}></div>
 
-        {/* Updated the hamburger icon to prevent default behavior */}
-        <div className="nav-link" onClick={handleMenuClick}>
+        <a href="#hdrmobmenu" className="nav-link" onClick={toggleMobileMenu}>
           <i className="fas fa-bars"></i>
-        </div>
+        </a>
       </div>
 
       {/* Show MobileMenu component if on mobile view */}
       {isMobileView && isMobileMenuOpen && <MobileMenu />}
+
+      {/* Show the mobile dropdown menu content when the hamburger is clicked */}
+      {isMobileMenuOpen && !isMobileView && (
+        <div className="mobile-menu">
+          {/* Mobile menu content */}
+          <div className="mContent">
+            <Products />
+            <Menu />
+          </div>
+
+          {/* Blue backdrop at the bottom */}
+          <div className="blue-backdrop"></div>
+
+          <button onClick={toggleMobileMenu} className="mobile-menu-close">
+            &times;
+          </button>
+        </div>
+      )}
     </nav>
   );
 };
